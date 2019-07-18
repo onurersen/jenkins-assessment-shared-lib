ae2eeae233307c9b3d48626aed1533bd1eff5412
Berat Onur Ersen <onurersen@gmail.com>
2019-07-18 14:10:41 +0300

    cleanup and comment for BuildConfig

--- src/com/onurersen/jenkins/BuildConfig.groovy
+++ src/com/onurersen/jenkins/BuildConfig.groovy
@@ -1,6 +1,16 @@
 package com.onurersen.jenkins
 import com.cloudbees.groovy.cps.NonCPS
 
+/**
+* This Groovy code 
+* helps to handle parameters coming from 
+* pipeline script and being used in shared library functionalities
+*
+* @author  Berat Onur Ersen
+* @version 1.0
+* @since   2019-07-18 
+*/
+
 class BuildConfig implements Serializable {
     static Map resolve(def body = [:]) {
 
